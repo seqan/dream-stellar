3a3d56fc9f7f15dddfd60ec9e222feff89a8aaf5
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-07-09 17:15:41 +0200

    [MISC] Remove debugging details

--- src/search/minimiser_model.cpp
+++ src/search/minimiser_model.cpp
@@ -10,9 +10,6 @@
  * \brief Provides stuff.
  */
 
-
-#include <seqan3/core/debug_stream.hpp>
-
 #include <numeric>
 
 #include <cereal/types/vector.hpp>
@@ -64,8 +61,6 @@ std::vector<size_t> precompute_threshold(size_t const pattern_size,
 
             if (n >= tau)
             {
-		// evelin debugging
-		seqan3::debug_stream << "Threshold: " << std::to_string(number_of_minimizers - i) << '\t';
     		thresholds.push_back(number_of_minimizers - i);
                 break;
             }
