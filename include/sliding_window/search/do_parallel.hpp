7213bd20109584142c3b276b97972bbd1ce2fad2
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-11-08 12:59:05 +0100

    Separate writing results from search

--- include/sliding_window/search/do_parallel.hpp
+++ include/sliding_window/search/do_parallel.hpp
@@ -4,6 +4,8 @@
 #include <future>
 #include <vector>
 
+#include <seqan3/core/debug_stream.hpp>
+
 namespace sliding_window
 {
 
@@ -11,7 +13,7 @@ template <typename t>
 inline void do_parallel(t && worker, size_t const num_records, size_t const threads, double & compute_time)
 {
     auto start = std::chrono::high_resolution_clock::now();
-    std::vector<decltype(std::async(std::launch::async, worker, size_t{}, size_t{}))> tasks;
+    std::vector<decltype(std::async(std::launch::async, worker, size_t{}, size_t{}))> tasks; // std::async returns a std::future
     size_t const records_per_thread = num_records / threads;
 
     for (size_t i = 0; i < threads; ++i)
@@ -22,10 +24,20 @@ inline void do_parallel(t && worker, size_t const num_records, size_t const thre
     }
 
     for (auto && task : tasks)
-        task.wait();
+    {
+        //task.wait();
 
+        //std::set<size_t> result_set = task.get();
+        auto result_set = task.get();
+        for (size_t bin : result_set)
+        {
+            seqan3::debug_stream << std::to_string(bin) << '\t';
+        }
+    }
+        
     auto end = std::chrono::high_resolution_clock::now();
     compute_time += std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
 }
 
 } // namespace sliding_window
+
