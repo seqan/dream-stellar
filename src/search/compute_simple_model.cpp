88bfa9eb0c6ca163b7914e159b391faaf466c956
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-12-20 18:32:25 +0100

    Separate minimiser threshold calculations

--- src/search/compute_simple_model.cpp
+++ src/search/compute_simple_model.cpp
@@ -3,22 +3,8 @@
 namespace sliding_window
 {
 
-std::vector<size_t> compute_simple_model(search_arguments const & arguments)
-{
-    std::vector<size_t> precomp_thresholds;
+std::vector<size_t> compute_simple_model(search_arguments const & arguments);
 
-    if (!arguments.threshold && !do_cerealisation_in(precomp_thresholds, arguments))
-    {
-        precomp_thresholds = precompute_threshold(arguments.pattern_size,
-                                                  arguments.window_size,
-                                                  arguments.kmer_size,
-                                                  arguments.errors,
-                                                  arguments.tau);
-
-        do_cerealisation_out(precomp_thresholds, arguments);
-    }
-
-    return precomp_thresholds;
-}
+threshold make_threshold_data(search_arguments const & arguments);
 
 } // namespace sliding_window
