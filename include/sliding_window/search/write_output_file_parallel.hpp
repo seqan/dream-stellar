7440854a12de4cf96def404324ba9c787cdcc5a6
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2022-01-16 14:25:32 +0100

    pass only (un-)compressed in worker definition instead of complete ibf type

--- include/sliding_window/search/write_output_file_parallel.hpp
+++ include/sliding_window/search/write_output_file_parallel.hpp
@@ -4,6 +4,8 @@
 #include <future>
 #include <vector>
 
+#include <seqan3/search/dream_index/interleaved_bloom_filter.hpp>
+
 #include <sliding_window/shared.hpp>
 #include <sliding_window/search/compute_simple_model.hpp>
 #include <sliding_window/search/query_result.hpp>
@@ -15,12 +17,22 @@ namespace sliding_window::app
 
 // && worker means only r-value references (temporary objects) can be passed
 // which means the memory can be reallocated after the function exits
-template <typename worker_t, typename ibf_t, typename rec_vec_t>
-inline void write_output_file_parallel(worker_t && worker, ibf_t const & ibf, search_arguments const & arguments, rec_vec_t const & records, 
-                            threshold const & threshold_data, sync_out & synced_out, double & compute_time)
+template <typename worker_t, seqan3::data_layout ibf_data_layout, typename rec_vec_t>
+inline void write_output_file_parallel(worker_t && worker,
+                                       seqan3::interleaved_bloom_filter<ibf_data_layout> const & ibf,
+                                       search_arguments const & arguments,
+                                       rec_vec_t const & records,
+                                       threshold const & threshold_data,
+                                       sync_out & synced_out,
+                                       double & compute_time)
 {
     auto start = std::chrono::high_resolution_clock::now();
-    std::vector<decltype(std::async(std::launch::async, worker, size_t{}, size_t{}, records, ibf, arguments, threshold_data))> tasks;
+
+    using task_future_t = std::future<std::vector<sliding_window::query_result>>;
+    static_assert(std::same_as<task_future_t,
+                               decltype(std::async(std::launch::async, worker, size_t{}, size_t{}, records, ibf, arguments, threshold_data))>);
+
+    std::vector<task_future_t> tasks;
     size_t const num_records = records.size();
     size_t const records_per_thread = num_records / arguments.threads;
 
@@ -30,8 +42,8 @@ inline void write_output_file_parallel(worker_t && worker, ibf_t const & ibf, se
         size_t const end = std::min(start + records_per_thread, num_records);
         tasks.emplace_back(std::async(std::launch::async, worker, start, end, records, ibf, arguments, threshold_data));
     }
-    
-    for (auto && task : tasks)
+
+    for (task_future_t & task : tasks)
     {
         // auto result_set = task.get();
         std::string result_string{};
