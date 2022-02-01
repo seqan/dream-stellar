f43b21e505e24ff4a6556b33e60e5d21c8a0edf1
Evelin Aasna <evelin.aasna@fu-berlin.de>
2022-02-01 15:02:33 +0100

    Number of bins as power of two

--- src/valik_split.cpp
+++ src/valik_split.cpp
@@ -1,5 +1,6 @@
 #include <valik/argument_parsing/shared.hpp>
 #include <valik/shared.hpp>
+#include <valik/split/find_segment_length.hpp>
 #include <valik/split/reference_metadata.hpp>
 #include <valik/split/reference_segments.hpp>
 
@@ -17,10 +18,7 @@ void valik_split(split_arguments const & arguments)
     reference_metadata reference(arguments.ref_file, true);
     reference.to_file(arguments.ref_out);
 
-    // Divide reference to segments that have at least min_len (doesn't take overlap into account)
-    // TODO: round up number of bins to next power of two
-    size_t segment_len = std::max(reference.total_len / arguments.min_bins, arguments.min_len);
-
+    size_t segment_len = find_segment_length(reference.total_len, arguments);
     if (reference.total_len < segment_len)
     {
         throw seqan3::argument_parser_error{"The segment length is greater than the total length of the reference."};
