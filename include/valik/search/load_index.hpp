81ac1a526230398bde0cdf71d8f906af10344d91
Evelin Aasna <evelin.aasna@fu-berlin.de>
2022-02-24 16:22:20 +0100

    Remove deprecated header

--- include/valik/search/load_index.hpp
+++ include/valik/search/load_index.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <seqan3/std/filesystem>
+#include <filesystem>
 
 #include <valik/index.hpp>
 #include <valik/shared.hpp>
--- include/valik/search/search_time_statistics.hpp
+++ include/valik/search/search_time_statistics.hpp
@@ -2,6 +2,8 @@
 
 #include <filesystem>
 
+#include <valik/shared.hpp>
+
 namespace valik::app
 {
 
