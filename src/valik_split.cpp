fd942734682e6801f47a917f7ef455bd4f476475
Evelin Aasna <evelin.aasna@fu-berlin.de>
2022-12-05 17:57:43 +0100

    Write segment files

--- src/valik_split.cpp
+++ src/valik_split.cpp
@@ -8,7 +8,7 @@ namespace valik::app
 
 //-----------------------------
 //
-// Divide reference database into partially overlapping segments and write metadata files.
+// Divide reference database into partially overlapping segments.
 //
 //-----------------------------
 void valik_split(split_arguments const & arguments)
@@ -21,33 +21,34 @@ void valik_split(split_arguments const & arguments)
     reference_segments segments(arguments.bins, arguments.overlap, reference);
     segments.to_file(arguments.seg_out);
 
-    using traits_type = seqan3::sequence_file_input_default_traits_dna;
-    seqan3::sequence_file_input<traits_type> fin{arguments.ref_file};
+    // Write segment sequences into separate FASTA files
+    using sequence_file_t = seqan3::sequence_file_input<dna4_traits, seqan3::fields<seqan3::field::seq>>;
 
     using types = seqan3::type_list<std::vector<seqan3::dna4>, std::string>;
     using fields = seqan3::fields<seqan3::field::seq, seqan3::field::id>;
     using sequence_record_type = seqan3::sequence_record<types, fields>;
 
     int i = 0;
-    for (auto && record : fin)
+    for (auto && [seq] : sequence_file_t{arguments.ref_file})
     {
         // get the relevant segments for each reference
         auto ref_seg = [&](reference_segments::segment & seg) {return reference.sequences.at(i).id == seg.ref_id;};
         for (auto & seg : segments.members | std::views::filter(ref_seg))
         {
-            std::filesystem::path bin_file = arguments.ref_file;
-            std::filesystem::path new_stem = bin_file.stem();
-            new_stem += "_";
-            new_stem += std::to_string(seg.bin);
-            new_stem += bin_file.extension();
-            bin_file.replace_filename(new_stem);
+            std::filesystem::path seg_file = arguments.ref_file;
+            std::filesystem::path seg_stem = seg_file.stem();
+            seg_stem += "_";
+            seg_stem += std::to_string(seg.bin);
+            seg_stem += seg_file.extension();
+            seg_file.replace_filename(seg_stem);
 
             std::string id{seg.ref_id + "_" + std::to_string(seg.start) + "_" + std::to_string(seg.len)};
-            seqan3::dna4_vector sequence(&record.sequence()[seg.start], &record.sequence()[seg.start+seg.len]);
+            seqan3::dna4_vector seg_sequence(&seq[seg.start], &seq[seg.start+seg.len]);
+            assert(seg_sequence.size() == seg.len);
 
-            seqan3::sequence_file_output fout{bin_file};
+            seqan3::sequence_file_output fout{seg_file};
 
-            sequence_record_type record{std::move(sequence), std::move(id)};
+            sequence_record_type record{std::move(seg_sequence), std::move(id)};
             fout.push_back(record);
         }
         i++;
