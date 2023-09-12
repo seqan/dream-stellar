52756a1e91fa815e3d996681c87bd9a00dbca6d5
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-09-12 11:58:29 +0200

    Fix

--- test/data/create_output.sh
+++ test/data/create_output.sh
@@ -8,20 +8,20 @@ if ! which valik 2> /dev/null; then
 fi
 
 export VALIK_TMP=tmp/valik/my_dir
-#export VALIK_STELLAR=echo
-#export VALIK_MERGE=echo
-#
-#echo "### Running valik split ###"
-#./split/cli_test_output.sh
-#
-#echo "### Running valik build ###"
-#./build/cli_test_output.sh
-#
-#echo "### Running valik search ###"
-#./search/cli_test_output.sh
-#
-#echo "### Running valik consolidate ###"
-#./consolidate/cli_test_output.sh
+export VALIK_STELLAR=echo
+export VALIK_MERGE=echo
+
+echo "### Running valik split ###"
+./split/cli_test_output.sh
+
+echo "### Running valik build ###"
+./build/cli_test_output.sh
+
+echo "### Running valik search ###"
+./search/cli_test_output.sh
+
+echo "### Running valik consolidate ###"
+./consolidate/cli_test_output.sh
 
 export VALIK_STELLAR=stellar
 export VALIK_MERGE=cat
