f418fc218424f46a1d5d4560d3d6271a1e616a44
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-09-13 08:23:13 +0200

    Fixup

--- src/consolidate/consolidate_matches.cpp
+++ src/consolidate/consolidate_matches.cpp
@@ -8,8 +8,6 @@ void consolidate_matches(search_arguments const & arguments)
     auto ref_meta = database_metadata(arguments.ref_meta_path, false);
     auto matches = read_stellar_output(arguments.all_matches, ref_meta);
 
-    seqan3::debug_stream << std::string(arguments.all_matches) << '\t' << matches.size() << '\n';
-
     std::sort( matches.begin(), matches.end(), std::greater<stellar_match>());
     matches.erase( std::unique( matches.begin(), matches.end() ), matches.end() );
 
