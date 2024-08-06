0a44843ce7022e18fa091a1acee82fb96b29e13a
Evelin <evelin.aasna@fu-berlin.de>
2024-08-06 15:29:05 +0200

    Add copyright notices to each file

--- include/dream_stellar/io/import_sequence.hpp
+++ include/dream_stellar/io/import_sequence.hpp
@@ -1,3 +1,26 @@
+// ==========================================================================
+//                    STELLAR - SwifT Exact LocaL AligneR
+//                   http://www.seqan.de/projects/stellar/
+// ==========================================================================
+// Copyright (C) 2010-2012 by Birte Kehr
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your options) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ==========================================================================
+// Author: Birte Kehr <birte.kehr@fu-berlin.de>
+// ==========================================================================
+
 #pragma once
 
 #include <type_traits> 
@@ -71,8 +94,6 @@ inline bool _import_database_sequences(input_t const & file_input,
     {
         str_err << "WARNING: Unknown database file input\n";
     }
-
-
 
     str_out << "Loaded " << seq_count << " adapted database sequence" << ((seq_count > 1) ? "s." : ".") << std::endl;
     if (!ids_unique)
