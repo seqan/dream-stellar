763037f3a92f7bb620ab64c9dcfa2603d5f72516
Evelin Aasna <evelin.aasna@fu-berlin.de>
2025-01-16 10:54:48 +0100

    Limit the number of cat arguments in merge
    [Fix] (seg file count % 1024 == 0) edge case hangs

--- src/consolidate/merge_processes.cpp
+++ src/consolidate/merge_processes.cpp
@@ -8,31 +8,44 @@ bool merge_processes(search_arguments const & arguments,
                      app::execution_metadata & exec_meta,
                      env_var_pack const & var_pack)
 {
-    // merge metadata from all threads
-    exec_meta.merge(arguments, time_statistics);
-    std::vector<std::string> merge_process_args;
-    if (exec_meta.output_files.size() > 0)
-    {
-        merge_process_args.push_back(var_pack.merge_exec);
-        for (auto & path : exec_meta.output_files)
-            merge_process_args.push_back(path);
-    }
-    else
-    {
-        //!WORKAROUND: merge hangs if no valik matches found
-        merge_process_args.push_back("echo");
-        merge_process_args.push_back("-n");
-    }
-
-    external_process merge(merge_process_args);
-
+    bool successful_merge{true};
     std::filesystem::path merge_out_path;
     if (arguments.bin_path.size() > 1)  // no need to consolidate metagenome search
         merge_out_path = arguments.out_file;
     else
         merge_out_path = arguments.all_matches;
-    std::ofstream matches_out(merge_out_path);
+    std::ofstream matches_out(merge_out_path, std::ofstream::app);
 
+    // merge metadata from all threads
+    exec_meta.merge(arguments, time_statistics);
+    std::vector<std::string> merge_process_args;
+    merge_process_args.push_back("echo");
+    if (exec_meta.output_files.size() > 0)
+    {
+        merge_process_args.push_back(var_pack.merge_exec);
+        for (auto & path : exec_meta.output_files)
+        {
+            merge_process_args.push_back(path);
+            if (merge_process_args.size() % 1024 == 0)
+            {
+                external_process merge(merge_process_args);
+                matches_out << merge.cout();
+                successful_merge = successful_merge && check_external_process_success(merge_process_args, merge);
+                merge_process_args.clear();
+                merge_process_args.push_back(var_pack.merge_exec);
+            }
+        }            
+    }
+
+    //!WORKAROUND: merge hangs if no valik matches found or no cat arguments after clear
+    if ((exec_meta.output_files.size() == 0) || (merge_process_args.size() == 1))
+    {
+        merge_process_args.clear();
+        merge_process_args.push_back("echo");
+        merge_process_args.push_back("-n");
+    }
+
+    external_process merge(merge_process_args);
     matches_out << merge.cout();
 
     for (auto & path : exec_meta.output_files)
@@ -51,7 +64,7 @@ bool merge_processes(search_arguments const & arguments,
             	std::cerr << "Could not delete folder: " << std::string(stellar_out_dir) << '\n';
     	}
     }
-    return check_external_process_success(merge_process_args, merge);
+    return successful_merge && check_external_process_success(merge_process_args, merge);
 }
 
 }   // namespace valik
