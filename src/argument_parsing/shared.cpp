1885f4b09af13938c5d1d83d19b63a934d0ee43f
Evelin Aasna <evelin.aasna@fu-berlin.de>
2025-06-17 14:40:39 +0200

    Rename executable to dream-stellar

--- src/argument_parsing/shared.cpp
+++ src/argument_parsing/shared.cpp
@@ -5,7 +5,7 @@ namespace valik::app
 
 void init_shared_meta(sharg::parser & parser)
 {
-    parser.info.app_name = "valik";
+    parser.info.app_name = "dream-stellar";
     parser.info.author = "Evelin Aasna";
     parser.info.email = "evelin.aasna@fu-berlin.de";
     parser.info.long_copyright = R"(BSD 3-Clause License
@@ -38,8 +38,8 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.)";
     parser.info.short_copyright = "BSD 3-Clause License";
-    parser.info.short_description = "Pre-filter for querying databases of nucleotide sequences for approximate local matches.";
-    parser.info.url = "https://github.com/eaasna/valik";
+    parser.info.short_description = "DNA search tool for finding local alignments between long sequences.";
+    parser.info.url = "https://github.com/seqan/dream-stellar";
     parser.info.version = "1.0";
 }
 
--- src/argument_parsing/top_level.cpp
+++ src/argument_parsing/top_level.cpp
@@ -8,7 +8,7 @@ void init_top_level_parser(sharg::parser & parser)
     init_shared_meta(parser);
     parser.info.description.emplace_back("Find local alignments between sets of DNA sequences.");
 
-    parser.info.examples = {"./valik build --help", "./valik search --help"};
+    parser.info.examples = {"./dream-stellar build --help", "./dream-stellar search --help"};
 }
 
 } // namespace valik::app
